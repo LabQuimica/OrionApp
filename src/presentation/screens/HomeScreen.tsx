279d90e7579ce36b7a497cbd2bfb2c3093d999d8
Nancy <nan.galicia03@gmail.com>
2024-05-15 15:52:56 -0600

    Falata integrar el nombre del usuarios

--- src/presentation/screens/HomeScreen.tsx
+++ src/presentation/screens/HomeScreen.tsx
@@ -1,35 +1,85 @@
-import { Text } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import  Ionicons  from '@expo/vector-icons/Ionicons'
 import { useEffect, useState } from 'react';
 import { supabase } from '../../../lib/supabase';
+import { Calendar } from 'react-native-calendars';
+import { QueryResult, QueryData, QueryError } from '@supabase/supabase-js'
 
 const HomeScreen = () => {
-
-  const [data, setData] = useState<any>(null)
-
+  const [selectedDate, setSelectedDate] = useState('');
+  const [valesWithPracticas, setValesWithPracticas] = useState<any[]>([]);
+  
   useEffect(() => {
-
     const fetchData = async () => {
-      const { data, error} = await supabase.from('kits').select('*')
-      if (error) {
-        console.log('error', error)
-      } else {
-        setData(data)
+      try {
+        const { data, error } = await supabase
+          .from('vales')
+          .select('id, id_usuario, practicas(id, nombre),fecha');
+        if (error) {
+          throw error;
+        }
+        setValesWithPracticas(data); 
+      } catch (error) {
+        console.error('Error al obtener datos:', error.message);
       }
-    }
+    };
+    fetchData();
+  }, []);
 
-    fetchData()
-  }, [])
 
-  console.log(JSON.stringify(data))
+  const markedDates = valesWithPracticas.reduce((acc, event) =>{
+    acc[event.fecha] = {marked: true, dotColor : 'red'};
+    return acc;
+  },{});
+
+  const eventsForSelectedDate = valesWithPracticas.filter(event => event.fecha === selectedDate);
+  console.log(JSON.stringify(valesWithPracticas))
 
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-black">
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
     <Ionicons name="airplane-outline" color={'white'} size={30} />
-    <Text className='text-white'>{JSON.stringify(data)}</Text>
+    
+    <View className="justify-center items-center">
+     
+     <Calendar
+          onDayPress={(day) => {
+            setSelectedDate(day.dateString);
+          }}
+          markedDates={{
+            ...markedDates,
+            [selectedDate]: { selected: true, selectedColor: 'blue' },
+          }}
+      />
+      <ScrollView style={styles.eventsContainer}>
+        {eventsForSelectedDate.map((event, index) => (
+          <View key={index} style={styles.event}>
+            <Text  >{event.practicas.nombre}</Text>
+            <Text  >{event.id_usuario}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
   </SafeAreaView>
+
   )
 }
 
 export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  eventsContainer: {
+    marginTop: 10,
+    padding: 10,
+  },
+  event: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    backgroundColor:'blue',
+    
+  },
+});
