0c364f307634dff60d6bc5de05b4a0902c0014cb
Nancy <nan.galicia03@gmail.com>
2024-06-23 20:02:05 -0600

    Cambios

--- src/presentation/screens/PracticasScreen.tsx
+++ src/presentation/screens/PracticasScreen.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Modal } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { supabase } from '../../../lib/supabase'; // Assuming supabase is initialized elsewhere
 import { Session } from '@supabase/supabase-js'
 const PracticasScreen = ({ route }) => {
   const { session } = route.params;
   const [valesWithPracticas, setValesWithPracticas] = useState<any[]>([]);
+  const [touchableDimensions, setTouchableDimensions] = useState({ width: 0, height: 0 });
+  const [openStartDatePicker, setOpenStartDatePiker] = useState(false);
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,7 +20,7 @@ const PracticasScreen = ({ route }) => {
       try {
         const { data, error } = await supabase
           .from('vales_2')
-          .select('id,practicas(id, nombre), fecha, estado')
+          .select('id,practicas(id, nombre), fecha, estado, id_profesor')
           .eq('id_usuario', session.user.id);
 
         if (error) {
@@ -38,33 +41,60 @@ const PracticasScreen = ({ route }) => {
 
   const getColorByState = (estado) => {
     switch (estado) {
-        case 0:
-            return 'rgba(255, 0, 0, 0.6)'; // Rojo con 50% de transparencia
-        case 1:
-            return 'rgb(240, 180, 70)'; // Amarillo con 50% de transparencia
-        case 2:
-            return 'rgba(0, 128, 0, 0.6)'; // Verde con 50% de transparencia
-        default:
-            return 'rgba(255, 255, 255, 0.6)'; // Blanco con 50% de transparencia
+      case 0:
+        return 'rgba(255, 0, 0, 0.6)'; // Rojo con 50% de transparencia
+      case 1:
+        return 'rgb(240, 180, 70)'; // Amarillo con 50% de transparencia
+      case 2:
+        return 'rgba(0, 128, 0, 0.6)'; // Verde con 50% de transparencia
+      default:
+        return 'rgba(255, 255, 255, 0.6)'; // Blanco con 50% de transparencia
     }
-};
+  };
 
+  const onTouchableLayout = (event) => {
+    const { width, height } = event.nativeEvent.layout;
+    setTouchableDimensions({ width, height });
+  };
+  const dynamicMargins = {
+    marginHorizontal: touchableDimensions.width * 0.05, // Ejemplo: 5% del ancho
+    marginVertical: touchableDimensions.height * 0.1, // Ejemplo: 5% del alto
+    height: touchableDimensions.height * 0.8, // Ejemplo: 90% del alto
+  };
+
+  const [count, setCount] = useState(0);
+  const onPress = () => setOpenStartDatePiker(!openStartDatePicker);
   const renderItem = ({ item }) => (
-    <View style={[styles.itemContainer, { backgroundColor: getColorByState(item.estado) }]}>
+    <TouchableOpacity style={[styles.itemContainer]} onPress={onPress} onLayout={onTouchableLayout}>
+      <View style={[styles.itemContainer2, { backgroundColor: getColorByState(item.estado) }, dynamicMargins]}></View>
       <Text style={styles.title} >Práctica: {item.practicas.nombre ?? 'N/A'}</Text>
       <Text style={styles.subtitle}>Fecha: {item.fecha ?? 'N/A'}</Text>
-    </View>
+    </TouchableOpacity>
   );
 
   return (
     <SafeAreaView style={styles.container}>
-      
+
       <FlatList
         data={valesWithPracticas}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
         contentContainerStyle={styles.listContent}
       />
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={openStartDatePicker}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>Despliegue de materiales por practica</Text>
+            <TouchableOpacity style={styles.closeButton} onPress={() => setOpenStartDatePiker(false)}>
+              <Text style={styles.closeButtonText}>Cerrar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+      </Modal>
     </SafeAreaView>
   );
 };
@@ -88,15 +118,57 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 10,
     borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#ccc',
+
   },
   title: {
-    color: 'white',
-    fontSize: 24, // Tamaño del texto del título
+    color: 'black',
+    fontSize: 20, // Tamaño del texto del título
     fontWeight: 'bold', // Peso de la fuente del título
   },
   subtitle: {
-    color: 'white',
+    color: 'black',
     fontSize: 18, // Tamaño del texto del subtítulo
     fontWeight: 'normal', // Peso de la fuente del subtítulo
   },
-});
+  itemContainer2: {
+    width: 15,
+    position: 'absolute',
+    right: 0,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 20,
+    elevation: 5,
+  },
+  closeButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 32,
+    borderRadius: 50,
+    elevation: 3,
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: 'black',
+  },
+  closeButtonText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+
+  },
+}
+);
